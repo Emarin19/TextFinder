4d86a3df15d4a599cc9d66902ef25837943d9a1e
Emarin19 <emarin702740530@gmail.com>
2019-10-03 14:54:28 -0600

    Prueba

--- src/main/java/cr/ac/tec/TextFinder/Main.java
+++ src/main/java/cr/ac/tec/TextFinder/Main.java
@@ -3,5 +3,7 @@ package cr.ac.tec.TextFinder;
 public class Main {
     public static void main(String[] args) {
         System.out.println("prueba");
+        System.out.println("Hola");
     }
+
 }
