0e67dade9ea71b779a5c7439c7f9daffb8e1f9b9
Emarin19 <emarin702740530@gmail.com>
2019-10-03 14:54:28 -0600

    Prueba

--- src/main/java/cr/ac/tec/TextFinder/Main.java
+++ src/main/java/cr/ac/tec/TextFinder/Main.java
@@ -1,31 +1,9 @@
 package cr.ac.tec.TextFinder;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.*;
-
-public class Main extends Application {
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("prueba");
+        System.out.println("Hola");
     }
 
-    /**
-     * Application start method (origin of application)
-     * @param primaryStage stage to be showed
-     * @throws IOException exception in case there's an error reading the fxml base file
-     */
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewModel.fxml"));
-        Parent root = loader.load();
-        primaryStage.setScene(new Scene(root, 1570, 810));
-        primaryStage.show();
-        ViewController controlGUI = loader.getController();
-        controlGUI.configureControl(primaryStage);
-    }
 }
