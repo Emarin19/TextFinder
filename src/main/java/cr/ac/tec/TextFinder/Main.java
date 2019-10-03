6aa9203a5076f734691c42dde9af9586308fce79
Emarin19 <emarin702740530@gmail.com>
2019-10-03 14:54:28 -0600

    Prueba

--- src/main/java/cr/ac/tec/TextFinder/Main.java
+++ src/main/java/cr/ac/tec/TextFinder/Main.java
@@ -1,12 +1,9 @@
 package cr.ac.tec.TextFinder;
 
-import cr.ac.tec.util.Collections.DataNode;
-import cr.ac.tec.util.Collections.TecList;
-
 public class Main {
     public static void main(String[] args) {
-        TecList<Integer> lol = new TecList<>();
-        lol.addAll(0,1,2,3,4,5,6);
-        System.out.println(lol.toString());
+        System.out.println("prueba");
+        System.out.println("Hola");
     }
+
 }
