c46c7a2de90ada8da3b87babf09714004c3cf0e6
Josfemova <49568644+Josfemova@users.noreply.github.com>
2019-10-05 12:31:42 -0600

    Agregada lista enlazada

--- src/main/java/cr/ac/tec/TextFinder/Main.java
+++ src/main/java/cr/ac/tec/TextFinder/Main.java
@@ -1,9 +1,12 @@
 package cr.ac.tec.TextFinder;
 
+import cr.ac.tec.util.Collections.TecList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("prueba");
-        System.out.println("Hola");
+        TecList<Integer> lol = new TecList<>();
+        lol.addAll(0,1,2,3,4,5,6);
+        System.out.println(lol.toString());
     }
 
 }
