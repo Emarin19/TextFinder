601d9b2879868d40ba8f7bb4dc361f4a32040957
Josfemova <49568644+Josfemova@users.noreply.github.com>
2019-10-06 18:30:49 -0600

    arreglado problema de cambino del main

--- src/main/java/cr/ac/tec/TextFinder/Main.java
+++ src/main/java/cr/ac/tec/TextFinder/Main.java
@@ -1,9 +1,31 @@
 package cr.ac.tec.TextFinder;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.*;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("prueba");
-        System.out.println("Hola");
+        launch(args);
     }
 
+    /**
+     * Application start method (origin of application)
+     * @param primaryStage stage to be showed
+     * @throws IOException exception in case there's an error reading the fxml base file
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewModel.fxml"));
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root, 1570, 810));
+        primaryStage.show();
+        ViewController controlGUI = loader.getController();
+        controlGUI.configureControl(primaryStage);
+    }
 }
